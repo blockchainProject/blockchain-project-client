c9ccb68545f41a7584cf2914cd88f19e09f0d282
Tai <stereophonics0215@gmail.com>
2016-06-27 12:09:26 +0900

    add icons in the left sidebar

--- js/views/leftSidebarVw.js
+++ js/views/leftSidebarVw.js
@@ -10,7 +10,7 @@ module.exports = baseVw.extend({
   el: '#leftSidebar',
 
   events:{
-  'click .leftSidebar-browse': 'navRefreshClick'
+  'click .leftSidebar-browse-title': 'navRefreshClick'
   },
 
   initialize: function(options){
